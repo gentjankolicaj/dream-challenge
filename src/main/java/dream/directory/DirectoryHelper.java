48650fcd4e584111ca9830af0ccb43eba986c227
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2021-03-05 21:39:37 +0100

    Implemented directoryHelper class for choosing destination directory for files.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

---
+++ src/main/java/dream/directory/DirectoryHelper.java
@@ -0,0 +1,28 @@
+package dream.directory;
+
+import javax.swing.*;
+import java.io.File;
+
+public class DirectoryHelper {
+
+    public static String chooseDirectory(JPanel jPanel) {
+        JFileChooser jFileChooser = new JFileChooser();
+        jFileChooser.setCurrentDirectory(new File("."));
+        jFileChooser.setDialogTitle("Choose file destination directory");
+        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        jFileChooser.setAcceptAllFileFilterUsed(false);
+
+        jFileChooser.setApproveButtonText("Save");
+        jFileChooser.setApproveButtonToolTipText("Save file at...");
+
+        if (jFileChooser.showOpenDialog(jPanel) == JFileChooser.APPROVE_OPTION) {
+            /**  System.out.println("Current directory "+jFileChooser.getCurrentDirectory());
+             System.out.println("Selected file "+jFileChooser.getSelectedFile());
+             System.out.println("Choose mode "+jFileChooser.getFileSelectionMode());
+             */
+            return jFileChooser.getSelectedFile().getPath();
+        } else
+            return null;
+    }
+
+}
--- src/main/java/dream/log/LogHelper.java
+++
@@ -1,4 +0,0 @@
-package dream.log;
-
-public class LogHelper {
-}
