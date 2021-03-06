feeb0bf69e6095c096291368053b4bf0515e4302
Gentjan Koliçaj <18643945+gentjankolicaj@users.noreply.github.com>
2021-03-06 21:19:56 +0100

    Pdf helper classes implementation.
    Signed-off-by: Gentjan Kolicaj <gentjankolicaj@yahoo.com>

--- src/main/java/dream/pdf/PdfHelper.java
+++ src/main/java/dream/pdf/PdfHelper.java
@@ -4,61 +4,52 @@ package dream.pdf;
 import dream.view.chance.ChanceAreaView;
 import dream.view.user.choice.ChoiceAreaView;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
 
 import javax.swing.*;
-import java.io.File;
 import java.io.IOException;
+
+import static dream.pdf.PdfTableHelper.drawConsoleTable;
+import static dream.pdf.PdfTableHelper.drawInputTable;
 
 public class PdfHelper {
 
-
     public static void exportToPdf(String destinationDirectory, int[] inputArray, JTextArea printArea, ChoiceAreaView[] choiceAreaViews, ChanceAreaView[] chanceAreaViews) {
-        PDDocument pdDocument = new PDDocument();
-        try {
+        if (destinationDirectory != null && !destinationDirectory.isEmpty()) {
+            PDDocument pdDocument = new PDDocument();
+            try {
 
-            PDPage[] inputPages = getInputPages(inputArray, printArea);
-            if (inputPages != null && inputPages.length != 0)
-                for (PDPage inputPage : inputPages)
-                    pdDocument.addPage(inputPage);
+                addInputPages(pdDocument, inputArray, printArea);
+                addChoicePages(pdDocument, choiceAreaViews);
+                addChancePage(pdDocument, chanceAreaViews);
 
+                String path = destinationDirectory + System.getProperty("file.separator") + "Export_results.pdf";
+                pdDocument.save(path);
 
-            PDPage[] choicePages = getChoicePages(choiceAreaViews);
-            if (choicePages != null && choicePages.length != 0)
-                for (PDPage choicePage : choicePages)
-                    pdDocument.addPage(choicePage);
+                pdDocument.close();
+            } catch (IOException e) {
+                e.printStackTrace();
 
-
-            PDPage[] chancePages = getChancePage(chanceAreaViews);
-            if (chancePages != null && chancePages.length != 0)
-                for (PDPage chancePage : chancePages)
-                    pdDocument.addPage(chancePage);
-
-
-            File exportFile = new File(destinationDirectory + System.getProperty("file.separator") + "Export_results.pdf");
-            pdDocument.save(exportFile);
-
-            pdDocument.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
     }
 
-    private static PDPage[] getChoicePages(ChoiceAreaView[] choiceAreaViews) {
-        return null;
+    private static void addChoicePages(PDDocument pdDocument, ChoiceAreaView[] choiceAreaViews) {
+        PdfTableHelper.drawChoiceTables(pdDocument, choiceAreaViews);
     }
 
-    private static PDPage[] getChancePage(ChanceAreaView[] chanceAreaViews) {
-        return null;
+    private static void addChancePage(PDDocument pdDocument, ChanceAreaView[] chanceAreaViews) {
+        PdfTableHelper.drawChanceTables(pdDocument, chanceAreaViews);
     }
 
 
-    private static PDPage[] getInputPages(int[] inputArray, JTextArea printArea) {
-        return null;
+    //todo : to fix console table extension
+    private static void addInputPages(PDDocument pdDocument, int[] inputArray, JTextArea printArea) {
+
+        PdfTableHelper.MultiField0 multiField0 = drawInputTable(pdDocument, inputArray);
+        drawConsoleTable(pdDocument, multiField0, printArea);
     }
 
 
